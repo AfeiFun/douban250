cb3ef32d4f39a91dbb2c12910f9a5964948775d8
BigFly <afforfun@outlook.com>
2020-08-17 14:55:59 +0800

    抓取数据并保存到json文件中

--- douban250.py
+++ douban250.py
@@ -1,43 +1,13 @@
-import requests
-from bs4 import BeautifulSoup
+import get_moives
 
-
-## 获取数据
-
-def get_movies_info_list(url,headers={},cookies={}):
-    
-    # 发起请求，获得response
-    r=requests.request('GET',url,headers=headers,cookies=cookies)
-
-    ## 解析数据
-    soup=BeautifulSoup(r.text,'html.parser')
-    # 找到所有的电影项目
-    movie_items=soup.find_all('div',class_='item')
-
-    #保存每个电影数据
-    # movie_lists=[]
-    # for movie_item in movie_items:
-        # movie_dict={}
-        # 查找电影名
-        movie_title=movie_items[0].find_next('span',class_='title').string
-        # movie_dict['movie_title']=movie_title
-        # 查找图片地址
-        movie_pic_address=movie_item[0].find_next('image')
-        print(movie_pic_address)
-        # movie_pic_address=movie_address['src']  
-    # return movie_lists
 
 def main():
-    url='https://movie.douban.com/top250'
-    headers={
-        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36 Edg/84.0.522.59'
-    }
-    cookies={'bid':'-iv9ZmYXAFM'}
-    
-    movie_lists=get_movies_info_list(url,headers=headers,cookies=cookies)
+    # 抓取数据并保存到json文件中
+    get_moives.get_all_page()
 
-if __name__ == "__main__":
-    
+    # 将json数据导出到数据库中
+
+if __name__ == "__main__":    
     main()
 
 
--- get_moives.py
+++ get_moives.py
@@ -1,25 +1,84 @@
 import requests
-import re
 from bs4 import BeautifulSoup
+import json
 
 
-url='https://movie.douban.com/top250'
-headers={
+def get_one_page(url):
+    '''
+    获取一页的数据并将数据加入到douban250.json文件中
+    '''
+    headers={
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36 Edg/84.0.522.59'
     }
-cookies={'bid':'-iv9ZmYXAFM'}
+    cookies={'bid':'-iv9ZmYXAFM'}
 
-r=requests.request('GET',url,headers=headers,cookies=cookies)
-soup=BeautifulSoup(r.text,'html.parser')
+    r=requests.request('GET',url,headers=headers,cookies=cookies)
+    soup=BeautifulSoup(r.text,'html.parser')
 
-movies=soup.find_all('div',class_='item')
-movie_title=movies[0].find_next('span',class_='title').string
-movie_pic=movies[0].find_next('img').attrs['src']
-movie_info=movies[0].p.text
-start=movie_info.index(':')
-end=movie_info.find('主')
-print(movie_info[start+1:end-3])
-# print(movie_info.find(':'))
-# movie_director=movie_info[4:movie_info.find(' ',4)]
+    movies=soup.find_all('div',class_='item')
+    movies_list=[]
+    for movie in movies:
+        
+        try:
+            #获取编号
+            movie_NO=movie.div.em.string
+            # #获取电影名字
+            movie_title=movie.find_next('span',class_='title').string
+            
+            # 获取图片下载地址
+            movie_pic_url=movie.find_next('img').attrs['src']
+            # 保存图片到文件夹
+            pic=requests.request('GET',movie_pic_url,headers=headers,cookies=cookies)
+            with open(r'pics/%s.webp'%movie_NO,'wb') as webp:
+                webp.write(pic.content)
+            # 获取图片路径
+            movie_pic=r'pics/%s.webp'%movie_NO
 
-# print(r.status_code,movie_title,movie_pic,movie_director)
+            #获取详情字符串
+            movie_info=movie.p.text.strip()
+            #截取导演字符串
+            start=4
+            end=movie_info.find(' ',start)
+            movie_director=movie_info[start:end]
+            #截取主演字符串
+            start=movie_info.index('主')+4
+            end=movie_info.find(' ',start)
+            movie_stars=movie_info[start:end]
+            #截取年份
+            movie_info=movie_info.replace(' ','')
+            start=movie_info.index('\n')+1
+            end=movie_info.find('/',start)-1
+            movie_year=movie_info[start:end]
+        except ValueError as reason:
+            movie_stars=''
+            print(movie_title,'抓取演员失败',reason)
+            pass
+        movie_dict={
+            '排名':movie_NO,
+            '电影名':movie_title,
+            '封面':movie_pic,
+            '导演':movie_director,
+            '主演':movie_stars,
+            '年份':movie_year
+        }
+        movies_list.append(movie_dict)
+        
+    # 保存数据到json文件
+    
+
+    file_name='douban250.json'
+
+    with open(file_name,'a') as file:
+        file=file.write(json.dumps(movies_list,ensure_ascii=False, indent=4, separators=(',', ':')))
+
+
+def get_all_page():
+    '''
+    获取所有的页面
+    '''
+    for i in range(10):
+        url='https://movie.douban.com/top250'
+        url=url+'?start=%d&filter='%(i*25)
+        print('正在下载第%d页'%(i+1))
+        get_one_page(url)
+    
